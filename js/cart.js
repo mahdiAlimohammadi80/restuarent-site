fd5ab09bc08ed364af80c04f72a3c46cd16d0f08
Mahdi <m.alimohammadi13802022@gmail.com>
2023-08-26 00:21:06 +0330

    add loadProduct function && showProduct function

--- js/cart.js
+++ js/cart.js
@@ -14,6 +14,62 @@ document.addEventListener('DOMContentLoaded',()=>{
     });
 })
 
+const cartItems = document.querySelector('.cart-items');
+let cartProduct = JSON.parse(localStorage.getItem('resCart')) || [] ;
+
+function showProduct(){
+    cartItems.innerHTML = '';
+    let totalCartPrice = 0;
+    cartProduct.forEach((pro)=>{
+        let TotalPrice = pro.count * pro.price;
+        totalCartPrice += TotalPrice;
+        let newItem = `<div class="card">
+        <div class="image-count">
+            <img src="${pro.image}" alt="تصویر آیتم منو" class="menu-card-image">
+        </div>
+        <div class="details">
+            <div class="title-count">
+                <h4>${pro.name}</h4>
+                <div class="count">
+                    <button class="increase-btn" onclick="increaseCount(this)">+</button>
+                    <input type="number" value="${pro.count}" disabled>
+                    <button class="decrease-btn" onclick="decreaseCount(this)">-</button>
+                </div>
+            </div>
+            <div class="price-count">
+                <div class="main-price">
+                    <span class="main-price-text">قیمت پایه :</span>
+                    <span class="price">${pro.price.toLocaleString()}</span>
+                </div>
+                <div class="main-count">
+                    <span class="main-price-text">تعداد کل:</span>
+                    <span>x<span class="count-text">${pro.count}</span></span>
+                </div>
+                <div class="Total-price">
+                    <span class="Total-price-text">قیمت نهایی :</span>
+                    <span class="Total">${TotalPrice.toLocaleString()}</span>
+                </div>
+            </div>
+        </div>
+       </div>`;
+       cartItems.innerHTML += newItem;
+    })
+
+    let cartTotalPrice = totalCartPrice; 
+    let cartCountText = 0; 
+
+    cartProduct.forEach((pro) => {
+        cartCountText += pro.count;
+    });
+
+    let cartTotalPriceText = cartTotalPrice.toLocaleString(); 
+    let resPriceText = cartTotalPrice.toLocaleString(); 
+
+    document.querySelector('.cart-count-text').textContent = cartCountText;
+    document.querySelector('.cartTotal-price-text').textContent = cartTotalPriceText;
+    document.querySelector('.res-price-text').textContent = resPriceText;
+}
+
 
 function addProduct(mainTitle) {
     let searchProduct = cartProduct.find((pro) => pro.name == mainTitle);
@@ -35,4 +91,17 @@ function addProduct(mainTitle) {
             }
         })
     }
+    showProduct();
 }
+
+function loadProduct() {
+    let getLocalData = JSON.parse(localStorage.getItem('resCart'));
+    if (getLocalData) {
+        cartProduct = JSON.parse(localStorage.getItem('resCart'));
+    } else {
+        cartProduct = [];
+    }
+    showProduct();
+}
+
+loadProduct();
